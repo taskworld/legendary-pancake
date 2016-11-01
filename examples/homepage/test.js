953cf12357368c2a21bfa8026d68593307ec1989
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-11-01 18:54:42 +0700

    Add some tests

---
+++ examples/homepage/test.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const fs = require('fs')
+const assert = require('assert')
+
+/* global describe, it */
+
+describe('webpack stats', () => {
+  exists('build/webpack.stats.json')
+})
+
+describe('generated pages', () => {
+  exists('build/pages/index.html')
+  contains('build/pages/index.html', 'page title', 'Home page - legendary-pancake')
+  contains('build/pages/index.html', 'rendered content', 'very customizable static site builder')
+  contains('build/pages/docs/index.html', 'markdown rendered content', 'Need inspiration?')
+})
+
+function exists (path) {
+  it(`${path} exists`, () => {
+    assert(fs.existsSync(path), `${path} must exist`)
+  })
+}
+
+function contains (path, thing, value) {
+  it(`${path} contains ${thing} (${value})`, () => {
+    assert(fs.readFileSync(path, 'utf8').includes(value), `${path} must contain ${value}`)
+  })
+}
